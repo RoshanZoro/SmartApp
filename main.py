bb0da69e2d1ad730a95ec9eba4e61b7a83c9e148
Roshan <roshan.kouwenhoven@student.hu.nl>
2025-09-08 16:22:08 +0200

    api

--- main.py
+++ main.py
@@ -10,20 +10,35 @@ cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
 retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
 openmeteo = openmeteo_requests.Client(session = retry_session)
 
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
 url = "https://api.open-meteo.com/v1/forecast"
 params = {
 	"latitude": 52.0908,
 	"longitude": 5.1222,
-	"hourly": ["temperature_2m", "relative_humidity_2m", "wind_speed_10m", "precipitation_probability", "precipitation", "cloud_cover"],
+	"hourly": ["temperature_2m", "relative_humidity_2m", "wind_speed_10m", "precipitation_probability", "precipitation", "cloud_cover", "apparent_temperature"],
 	"current": "temperature_2m",
 	"timezone": "Europe/Berlin",
 	"forecast_days": 1,
 	"forecast_hours": 1,
 }
 responses = openmeteo.weather_api(url, params=params)
+
+# Process first location. Add a for-loop for multiple locations or weather models
 response = responses[0]
+print(f"Coordinates: {response.Latitude()}°N {response.Longitude()}°E")
+print(f"Elevation: {response.Elevation()} m asl")
+print(f"Timezone: {response.Timezone()}{response.TimezoneAbbreviation()}")
+print(f"Timezone difference to GMT+0: {response.UtcOffsetSeconds()}s")
+
+# Process current data. The order of variables needs to be the same as requested.
 current = response.Current()
 current_temperature_2m = current.Variables(0).Value()
+
+print(f"\nCurrent time: {current.Time()}")
+print(f"Current temperature_2m: {current_temperature_2m}")
+
+# Process hourly data. The order of variables needs to be the same as requested.
 hourly = response.Hourly()
 hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
 hourly_relative_humidity_2m = hourly.Variables(1).ValuesAsNumpy()
@@ -31,6 +46,7 @@ hourly_wind_speed_10m = hourly.Variables(2).ValuesAsNumpy()
 hourly_precipitation_probability = hourly.Variables(3).ValuesAsNumpy()
 hourly_precipitation = hourly.Variables(4).ValuesAsNumpy()
 hourly_cloud_cover = hourly.Variables(5).ValuesAsNumpy()
+hourly_apparent_temperature = hourly.Variables(6).ValuesAsNumpy()
 
 hourly_data = {"date": pd.date_range(
 	start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
@@ -39,11 +55,25 @@ hourly_data = {"date": pd.date_range(
 	inclusive = "left"
 )}
 
+hourly_data["temperature_2m"] = hourly_temperature_2m
+hourly_data["relative_humidity_2m"] = hourly_relative_humidity_2m
+hourly_data["wind_speed_10m"] = hourly_wind_speed_10m
+hourly_data["precipitation_probability"] = hourly_precipitation_probability
+hourly_data["precipitation"] = hourly_precipitation
+hourly_data["cloud_cover"] = hourly_cloud_cover
+hourly_data["apparent_temperature"] = hourly_apparent_temperature
+
+hourly_dataframe = pd.DataFrame(data = hourly_data)
+print("\nHourly data\n", hourly_dataframe)
+
 unixTime = current.Time()
 temp = int(current_temperature_2m)
 utc_time = datetime.fromtimestamp(unixTime, UTC)
 timeFormat = utc_time.strftime("%Y-%m-%d %H:%M:%S %Z")
 regen = int(hourly_precipitation)
+gevoelTemp = int(hourly_apparent_temperature)
+
 print("UTC:", timeFormat)
 print("Het is momenteel", round(temp) , " graden")
+print("De gevoelstemperatuur is", round(gevoelTemp))
 print("Er is momenteel" , regen, "% kans op regen")
