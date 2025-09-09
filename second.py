56f00214cea5595342380ec642263775d276dfca
Roshan <roshan.kouwenhoven@student.hu.nl>
2025-09-09 09:47:00 +0200

    api

--- second.py
+++ second.py
@@ -1,8 +1,36 @@
-tempCel = int( input("Wat is de temperatuur? ") )
-windSnel = int( input("Wat is de windsnelheid? "))
-vocht = int( input("Wat is de vochtigheid percentage? "))
-tempFahr = 1.8 * tempCel + 32
-gevoelsTemp = tempCel - (vocht / 100) * windSnel
-print(f"Het is momenteel {tempCel} graden celcius.")
-print(f"Het is momenteel {tempFahr} graden fahrenheit.")
-print(f"De gevoelstemperatuur is momenteel {gevoelsTemp} graden.")
+
+#8.2 Het creëren van functies
+def gevoelsTemp (tempCel, vocht, windSnel):
+    return tempCel - (vocht / 100) * windSnel
+def tempFahr (tempCel):
+    return 1.8 * int(tempCel) + 32
+def airco(tempCel):
+    if int(tempCel) > 25:
+        return "Het is warm. De airco kan aan."
+    else
+        return "Het is niet warm."
+
+dagen = 0
+#f-string, 44 Eenvoudige Functies
+
+while True: #7.5 De loop-en-een-half 89
+    inp = input(f"Wat is de temperatuur op dag {dagen + 1}? [C] ")
+    if inp == "":
+        print("Je moet nog een getal invoeren!")
+    else:
+        tempCel = int(inp)
+        if inp == "":
+            print("Je moet nog een getal invoeren!")
+        else:
+            windSnel = int( input("Wat is de windsnelheid? [m/s]"))
+        if inp == "":
+            print("Je moet nog een getal invoeren!")
+        else:
+            vocht = int( input("Wat is de vochtigheid percentage? [%] "))
+
+        temp = float(inp)
+        fahrenheit = tempFahr(inp)
+
+    print(f"Het is momenteel {tempCel} graden celcius.")
+    print(f"Het is momenteel {fahrenheit} graden fahrenheit.")
+    print(f"De gevoelstemperatuur is momenteel {gevoelsTemp} graden.")
